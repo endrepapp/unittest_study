b0b561f571408e299c93e33b4ebb0b38a3e1e6d1
Gergely Máté <gergelymate@Gergelys-MacBook-Pro.local>
2016-07-22 21:17:37 +0200

    files added

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from math import *
+
+# Write code for test
+#
+# All tests should be passed
+    # write 'prime_check' function which input is a integer
+    # and return True or False value
+    # Értsd ellenőrízze, hogy az adott szám prim szám e
+
+    # write 'square_number_check' function which input is a integer
+    # and return True or False value
+
+def main():
+
+    return
+
+if __name__ == '__main__':
+    main()
